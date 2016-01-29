ca2ed0216cf04b684374396f20e49f3f3fd6cd54
DL_Ben <lifeisgonnabeok@hotmail.com>
2016-01-29 12:28:50 -0800

    Create wordcount_mapper.py
    map function for word count

---
+++ wordcount_mapper.py
@@ -0,0 +1,18 @@
+
+import sys     
+for line in sys.stdin:  
+
+#-----------------------------------
+#sys.stdin call 'sys' to read a line from standard input, 
+# note that 'line' is a string object, ie variable, and it has methods that you can apply to it,
+# as in the next line
+# ---------------------------------
+    line = line.strip()  #strip is a method, ie function, associated
+                         #  with string variable, it will strip 
+                         #   the carriage return (by default)
+    keys = line.split()  #split line at blanks (by default), 
+                         #   and return a list of keys
+    for key in keys:     #a for loop through the list of keys
+        value = 1        
+        print('{0}\t{1}'.format(key, value) ) #the {} is replaced by 0th,1st items in format list
+                            #also, note that the Hadoop default is 'tab' separates key from the valu
